1f8fb786cad35ba7cee1d7768902db6ef1924cd0
raju <rajurevalla121@gmail.com>
2023-04-06 07:53:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/players/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/players/", async (request, response) => {
+  const getAllPlayers = `
+                        select
+
+                        player_name 
+                        from
+                         
+                         cricket_team
+    `;
+
+  const getPlayers = await db.get(getAllPlayers);
+
+  response.send(getPlayers);
+});
